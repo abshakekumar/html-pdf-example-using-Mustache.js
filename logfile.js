2ac19710c87c88f32d97063eaf1f771b2dc395aa
abshakekumar <abhishek.mcs.du.2014@gmail.com>
2018-07-08 16:28:36 +0530

    removed logger file

--- logfile.js
+++
@@ -1,8 +0,0 @@
-
-var logObject = {
-	info : function (msg) { console.log('INFO :', msg);},
-	warn : function (msg) { console.log('WARN :', msg);},
-	error : function (msg) { console.log('ERROR :', msg);}
-};
-
-module.exports = logObject;
