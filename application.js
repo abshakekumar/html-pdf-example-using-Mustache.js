05f6b12c154221ae52018bf0ddeb1f2f40d84be2
abshakekumar <abhishek.mcs.du.2014@gmail.com>
2018-07-08 16:29:51 +0530

    deleted usage of logger module file

--- application.js
+++
@@ -1,5 +0,0 @@
-var logModule = require('./logfile.js');
-
-logModule.warn('warning created at the line');
-logModule.error('error created at the line');
-logModule.info('info created at the line');
